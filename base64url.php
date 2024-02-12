4f01d74a2c10d0c54a4ce6f038573f1ed1cc421d
antikirra <antikirra@gmail.com>
2024-02-12 21:21:32 +0700

    FIX

--- base64url.php
+++ base64url.php
@@ -2,20 +2,25 @@
 
 namespace Antikirra;
 
-/**
- * @param string $string
- * @return string
- */
-function base64url_encode($string)
-{
-    return rtrim(strtr(base64_encode($string), '+/', '-_'), '=');
+
+if (!function_exists('Antikirra\\base64url_encode')) {
+    /**
+     * @param string $string
+     * @return string
+     */
+    function base64url_encode($string)
+    {
+        return rtrim(strtr(base64_encode($string), '+/', '-_'), '=');
+    }
 }
 
-/**
- * @param string $string
- * @return string|false
- */
-function base64url_decode($string)
-{
-    return base64_decode(strtr($string, '-_', '+/') . str_repeat('=', 3 - (3 + strlen($string)) % 4));
+if (!function_exists('Antikirra\\base64url_decode')) {
+    /**
+     * @param string $string
+     * @return string|false
+     */
+    function base64url_decode($string)
+    {
+        return base64_decode(strtr($string, '-_', '+/') . str_repeat('=', 3 - (3 + strlen($string)) % 4));
+    }
 }
